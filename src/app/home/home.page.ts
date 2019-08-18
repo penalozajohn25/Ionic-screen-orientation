ff4525c2afa881625d725bbcddb5df68a5f168e5
penalozajohn25 <johnmanuelpenaloza@gmail.com>
2019-08-18 23:49:50 +0000

    screen orientation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx'
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public currentScreenOrientation: string;
+  constructor(private screenOrientation: ScreenOrientation) {
+     // detecto la orientacion actual
+     this.currentScreenOrientation = this.screenOrientation.type;
+ 
+     // decto cuando la orientacion cambie
+     this.screenOrientation.onChange().subscribe(
+       () => {
+         alert("Orientation Changed"+this.screenOrientation.type);
+         this.currentScreenOrientation = this.screenOrientation.type;
+       }
+     );
+  }
 
-  constructor() {}
+  public setLandscape(){
+    // cambiar a horizontal
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+  }
+ 
+  public setPortrait(){
+    // cambiar a vertical
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
+  }
+
+  public unlockScreen(){
+    // desbloquear orientacion actual
+    this.screenOrientation.unlock();
+  }
 
 }
